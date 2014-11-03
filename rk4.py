700500bd10d5927b264ca2ff264c9571051cc78f
Emi! <emifixthis@gmail.com>
2014-11-03 01:16:21 -0800

    default function change, error can be non default
    changed default function value
    added descriptive commenting as per request
    User can now specify a non default error function

--- rk4.py
+++ rk4.py
@@ -1,8 +1,8 @@
 #! usr/bin/env python
 
-from math import sqrt, fabs
+from math import sqrt, fabs, exp
 
-
+#   This is the calculation of f
 def rk4Step(f, dt, t, y):
     k1 = dt*f(t,y)
     k2 = dt*f(t+dt/2, y+ k1/2)
@@ -11,20 +11,23 @@ def rk4Step(f, dt, t, y):
     return y + (k1 + 2*k2 + 2*k3 + k4)/6
 
 
-def getY2(t):
-    return (t**2/4.0 + 1)**2
+#   This is 'Truth' (aka the actual solution as given by the book)
+def e(t):
+    return sqrt(4 - 3*exp(-t**2)
 
-
+#   This returns the error
+#   Note it is writen as a ratio but it does not matter because (y/y2 - 1) = (y - y2)
 def getError(y, y2):
     return fabs(y/y2 - 1.0)
 
-
+#   Printer
 def printLine(t, y, error, delim=' '*5):
     print 'y({:.1f}){}{:.7f}{}error: {}'\
           .format(t, delim, y, delim, error)
     
-
-def rk4(f=lambda a, b: a*sqrt(b), steps=10, t=0, y=1, dt=0.1, output=True):
+#   Smashes it all together
+#   Here rk4 is defined with default arguments that will be called if no alternate arguments are given by the user
+def rk4(f=lambda a, b: a*sqrt(b), e=lambda c, c: sqrt(4 - 3*exp(-t**2), steps=10, t=0, y=1, dt=0.1, output=True):
     
     if output is True:
         printLine(t, y, 0)
@@ -32,7 +35,7 @@ def rk4(f=lambda a, b: a*sqrt(b), steps=10, t=0, y=1, dt=0.1, output=True):
     for i in xrange(0, steps):
         y  =  rk4Step(f, dt, t, y)
         t  += dt
-        y2 =  getY2(t)
+        y2 =  e(t)
         if output is True:
             printLine(t, y, getError(y, y2))
 
