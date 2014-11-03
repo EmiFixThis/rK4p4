9b9fe58c2d21ec55c877408f67e4d6afd9181335
Emi! <emifixthis@gmail.com>
2014-11-03 01:33:00 -0800

    fixed syntax error

--- rk4.py
+++ rk4.py
@@ -2,6 +2,8 @@
 
 from math import sqrt, fabs, exp
 
+t = 0
+
 #   This is the calculation of f
 def rk4Step(f, dt, t, y):
     k1 = dt*f(t,y)
@@ -12,8 +14,8 @@ def rk4Step(f, dt, t, y):
 
 
 #   This is 'Truth' (aka the actual solution as given by the book)
-def e(t):
-    return sqrt(4 - 3*exp(-t**2)
+def aVal(t):
+    return sqrt(4 - 3*exp(-t**2))
 
 #   This returns the error
 #   Note it is writen as a ratio but it does not matter because (y/y2 - 1) = (y - y2)
@@ -27,7 +29,7 @@ def printLine(t, y, error, delim=' '*5):
     
 #   Smashes it all together
 #   Here rk4 is defined with default arguments that will be called if no alternate arguments are given by the user
-def rk4(f=lambda a, b: a*sqrt(b), e=lambda c, c: sqrt(4 - 3*exp(-t**2), steps=10, t=0, y=1, dt=0.1, output=True):
+def rk4(f=lambda a, b: sqrt(4 - 3*exp(-t**2)), steps=10, t=0, y=1, dt=0.1, output=True):
     
     if output is True:
         printLine(t, y, 0)
@@ -35,7 +37,7 @@ def rk4(f=lambda a, b: a*sqrt(b), e=lambda c, c: sqrt(4 - 3*exp(-t**2), steps=10
     for i in xrange(0, steps):
         y  =  rk4Step(f, dt, t, y)
         t  += dt
-        y2 =  e(t)
+        y2 =  aVal(t)
         if output is True:
             printLine(t, y, getError(y, y2))
 
